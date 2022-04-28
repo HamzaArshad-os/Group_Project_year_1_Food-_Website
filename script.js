d2566026a97573e9c8820e035978397a1bb2633c
HamzaArshad-os <hamza.sa2002@gmail.com>
2022-04-28 10:36:41 +0100

    hamza update
    Co-Authored-By: AsifMir97 <102671069+AsifMir97@users.noreply.github.com>

--- script.js
+++ script.js
@@ -77,6 +77,63 @@ function doSomething(input) {
 
 //////////
 
+const images = document.querySelectorAll(".carousel");
+      let i = 0;
+
+      setInterval(function(){
+        myFunctiongoForward()
+        
+        }, 2000);
+
+        
+
+      
+      setInterval(myFunctiongoForward, 2000); 
+      var myFunctiongoBack = function(){//works
+        if(i != 0 ){
+          images[i].style.display = 'none';
+          images[i-1].style.display = 'block';
+          i--;
+        }
+        else{
+          images[i].style.display = 'none';
+          images[images.length-1].style.display = 'block'; 
+          i = images.length -1 ; //try it now!
+        }
+      }
+        
+    var myFunctiongoForward = function(){//fixed
+      if(i != images.length-1){ 
+        images[i].style.display = 'none';
+        images[i+1].style.display = 'block';
+        i++;
+      }
+      else{
+        images[i].style.display = 'none';
+        images[0].style.display = 'block';
+        i = 0;
+      }
+
+      
+    }
+
+    document.addEventListener('keydown',logkey);
+        function logkey(e){
+          log.textContent;
+          
+          
+          if(e.code === "ArrowRight"){
+            
+            myFunctiongoForward()
+          }
+          if(e.code === "ArrowLeft"){
+         
+            myFunctiongoBack()
+          }
+        }
+
+///////////
+
 
  
  
